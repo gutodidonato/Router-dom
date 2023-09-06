6ed177b055a096ade676722316d86e7aa33448d7
gutodidonato <rm99433@fiap.com.br>
2023-09-05 21:06:00 -0300

    inicio projeto react

---
+++ my-create-app/src/main.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.jsx";
+import Home from "./routes/Home/Index.jsx";
+import Produtos from "./routes/Produtos/Index.jsx";
+import Error from "./routes/Error/index.jsx";
+import EditarProduto from "./routes/EditarProduto/Index.jsx";
+import "./index.css";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/produtos",
+        element: <Produtos />,
+      },
+      {
+        path: "/produtos/editar/:id",
+        element: <EditarProduto />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
