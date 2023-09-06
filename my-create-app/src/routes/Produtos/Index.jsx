a9b90bd4baca6903a41a00c340292615dd96d00d
gutodidonato <rm99433@fiap.com.br>
2023-09-05 21:43:00 -0300

    projeto funcionado

--- my-create-app/src/routes/Produtos/Index.jsx
+++ my-create-app/src/routes/Produtos/Index.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import { listaProdutos } from "../data/listaProdutos.js";
+import { listaProdutos } from "../../data/listaProdutos";
 
 export default function Produtos() {
   return (
