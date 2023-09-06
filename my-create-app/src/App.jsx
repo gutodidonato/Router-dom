f76bfc3312aab78a30456c62a45aac7ae4fffec5
gutodidonato <rm99433@fiap.com.br>
2023-09-05 21:20:28 -0300

    rodape no app

--- my-create-app/src/App.jsx
+++ my-create-app/src/App.jsx
@@ -1,12 +1,14 @@
 import Menu from "./Menu.jsx";
 import { Outlet } from "react-router-dom";
 import "./App.css";
+import Rodape from "./Rodape.jsx";
 
 function App() {
   return (
     <>
       <Menu />
       <Outlet />
+      <Rodape />
     </>
   );
 }
